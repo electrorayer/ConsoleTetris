3e62252075753293bea45169683d99b36b391e62
electrorayer <electrorayer@gmail.com>
2023-11-03 01:39:57 +0100

    added collision detection for all possible movement

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,16 @@ struct Shape {
     shape: &'static str,
     x: i32,
     y: i32,
+    len: i32,
 }
 
 impl Shape {
     fn new(shape: &'static str, x: i32, y: i32) -> Shape {
-        Shape { shape, x, y }
+        Shape { shape, x, y, len: shape.chars().count() as i32 }
+    }
+
+    fn bar() -> Shape {
+        Shape::new("      ", 3, 3)
     }
 }
 
@@ -31,41 +36,44 @@ fn main() {
     let mut elapsed_frames = 0;
 
     let mut inactive_shapes: Vec<Shape> = vec![];
-    let mut active_shape = Shape::new("####", 3, 3);
+    let mut active_shape = Shape::bar();
 
     loop {
         engine.wait_frame();
         engine.clear_screen();
-
         
         // define box
         engine.rect(0, 1, RECT_LOWER_X, RECT_LOWER_Y, pixel::pxl_bg(' ', Color::White));
 
-
         // print inactive shapes
         for shape in &inactive_shapes {
-            engine.print(shape.x, shape.y, shape.shape);
+            engine.print_fbg(shape.x, shape.y, shape.shape, Color::Blue, Color::Blue);
         }
 
         // print active shape
-        engine.print(active_shape.x, active_shape.y, active_shape.shape);
+        engine.print_fbg(active_shape.x, active_shape.y, active_shape.shape, Color::Blue, Color::Blue);
 
-        // go down one unit per second
-        if engine.frame_count % 60 == 0 {
-            if active_shape.y+1 < RECT_LOWER_Y {
+        // go down one unit per second or if s is pressed
+        if engine.frame_count % 60 == 0 || engine.is_key_pressed(KeyCode::Char('s')) {
+            if !has_collision(&inactive_shapes, &active_shape, "y+") {
                 active_shape.y += 1;
             } else {
+                // game over if stack too high
+                if active_shape.y == 3 {
+                    break
+                }
+
                 // hit something? -> retire active shape
                 inactive_shapes.push(active_shape);
     
                 // generate new active shape
-                active_shape = Shape::new("####", 3, 3)
+                active_shape = Shape::bar();
             }
         }
-        
 
-        // debug coordinates
+        // debug coordinates and shape counter
         engine.print(10, 0, format!("Y:{} X:{}", active_shape.y, active_shape.x).as_str());
+        engine.print(23, 0, format!("{}", inactive_shapes.len()).as_str());
 
         // FPS counter
         if stopwatch.elapsed().as_millis() >= Duration::from_millis(1000).as_millis() {
@@ -83,33 +91,88 @@ fn main() {
             break
         }
 
-        else if engine.is_key_pressed(KeyCode::Char('s')) {
-            if active_shape.y+1 < RECT_LOWER_Y {
-                active_shape.y += 1;
-            }
-        }
-
-        else if engine.is_key_pressed(KeyCode::Char('z')) {
-            if active_shape.y-1 > 1 {
-                active_shape.y -= 1;
-            }
-        }
-
+        // LEFT
         else if engine.is_key_pressed(KeyCode::Char('q')) {
-            if active_shape.x-1 > 0 {
+            if !has_collision(&inactive_shapes, &active_shape, "x-") {
                 active_shape.x -= 1;
             }
         }
 
+        // RIGHT
         else if engine.is_key_pressed(KeyCode::Char('d')) {
-            if active_shape.x + (active_shape.shape.len() as i32) < RECT_LOWER_X {
+            if !has_collision(&inactive_shapes, &active_shape, "x+") {
                 active_shape.x += 1;
             }
         }
-
+    
 
         engine.draw();
     }
 
     
+}
+
+fn has_collision(inactive_shapes: &Vec<Shape>, active_shape: &Shape, direction_change: &str) -> bool {
+    match direction_change {
+        // move right
+        "x+" => {
+            // hit right wall
+            if active_shape.x + active_shape.len >= RECT_LOWER_X {
+                return true
+            }
+
+            // 2 shapes can't have overlaying cells
+            for inactive_shape in inactive_shapes {
+                for i in 0..active_shape.len {
+                    for j in 0..inactive_shape.len {
+                        // can't have shared x pos on the same y pos
+                        if active_shape.x+active_shape.len == inactive_shape.x && active_shape.y == inactive_shape.y {
+                            return true
+                        }   
+                    }
+                }
+            }
+        },
+        // move left
+        "x-" => {
+            if active_shape.x-1 <= 0 {
+                return true
+            }
+
+            // 2 shapes can't have overlaying cells
+            for inactive_shape in inactive_shapes {
+                for i in 0..active_shape.len {
+                    for j in 0..inactive_shape.len {
+                        // can't have shared x pos on the same y pos
+                        if active_shape.x-1 == inactive_shape.x+inactive_shape.len-1 && active_shape.y == inactive_shape.y {
+                            return true
+                        }   
+                    }
+                }
+            }
+        },
+        // move down
+        "y+" => {
+            // hit bottom
+            if active_shape.y+1 >= RECT_LOWER_Y {
+                return true
+            }
+
+            // 2 shapes can't have overlaying cells
+            for inactive_shape in inactive_shapes {
+                for i in 0..active_shape.len {
+                    for j in 0..inactive_shape.len {
+                        // can't have shared x pos on the same y pos
+                        if active_shape.x + i == inactive_shape.x + j && active_shape.y+1 == inactive_shape.y {
+                            return true
+                        }   
+                    }
+                }
+            }
+            
+        },
+        _ => panic!("unknown direction change")
+    }
+
+    false
 }
